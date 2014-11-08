11b8c89e7e9b61a493c651b5f7d7384ae0171959
Daniel Rampelt <drampelt@gmail.com>
2014-11-07 21:48:46 -0500

    Add some zsh files

---
+++ home/.zshrc.d/x.zsh
@@ -0,0 +1,4 @@
+alias xclip="xclip -selection c"
+alias pbcopy="tee >(\\xclip) >(xclip)"
+alias volup="amixer -q set Master 2dB+ unmute && pkill -RTMIN+10 i3blocks"
+alias voldown="amixer -q set Master 2dB- unmute && pkill -RTMIN+10 i3blocks"
