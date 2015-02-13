63346795dd7eeef0e6d83d4f45f010d8f3fbc441
Daniel Rampelt <drampelt@gmail.com>
2015-02-13 16:19:56 -0500

    Better i3 gaps

--- home/.zshrc.d/x.zsh
+++ home/.zshrc.d/x.zsh
@@ -2,3 +2,4 @@ alias xclip="xclip -selection c"
 alias pbcopy="tee >(\\xclip) >(xclip)"
 alias volup="amixer -q set Master 2dB+ unmute && pkill -RTMIN+10 i3blocks"
 alias voldown="amixer -q set Master 2dB- unmute && pkill -RTMIN+10 i3blocks"
+export N_PREFIX=$HOME
