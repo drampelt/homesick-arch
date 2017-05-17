8b9b4d1ae87a91fcdeb68cf27b41c349a937b5fd
Daniel Rampelt <drampelt@gmail.com>
2017-05-17 02:07:25 -0400

    chore: minor updates

--- home/.i3/scripts/sparkmem.sh
+++ home/.i3/scripts/sparkmem.sh
@@ -3,4 +3,4 @@ not_apps=0
 for mem in $(egrep '(MemFree|Buffers|Cached|Slab|PageTables|SwapCached)' /proc/meminfo | egrep -o '[0-9]+'); do
     not_apps=$((not_apps+mem))
 done
-sparklines $((total-not_apps)) $total 0 | tail -n 1 | cut -b 1-3
+spark $((total-not_apps)) $total 0 | tail -n 1 | cut -b 1-3
