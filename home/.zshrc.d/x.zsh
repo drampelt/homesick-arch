2754ea0bd9698921974938941b91944c62484e5e
Daniel Rampelt <drampelt@gmail.com>
2017-07-25 15:44:21 -0400

    Improvements

--- home/.zshrc.d/x.zsh
+++ home/.zshrc.d/x.zsh
@@ -1,5 +1,7 @@
-alias xclip="xclip -selection c"
-alias pbcopy="tee >(\\xclip) >(xclip)"
+# alias xclip="xclip -selection c"
+# alias pbcopy="tee >(\\xclip) >(xclip)"
+alias pbcopy="xsel --clipboard --input"
+alias pbpaste="xsel --clipboard --output"
 alias volup="amixer -q set Master 2dB+ unmute && pkill -RTMIN+10 i3blocks"
 alias voldown="amixer -q set Master 2dB- unmute && pkill -RTMIN+10 i3blocks"
 export N_PREFIX=$HOME
